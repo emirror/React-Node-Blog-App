0aa61319c742f0d12eb541a0b1e4bcb8828d163b
Ehsan Aeineh <ehsan_aeineh@hotmail.com>
2025-11-02 19:30:25 +0800

    add cors

--- server/src/index.js
+++ server/src/index.js
@@ -3,7 +3,7 @@ import routes from "./routes/index.js" // Import routes
 import errorHandler from "./middlewares/error-handler.js";
 import { sequelize } from "./config/database.js";
 import User from "./models/user.js";
-
+import cors from "cors";
 const app = express();
 
 app.use(express.json());
@@ -15,7 +15,7 @@ await sequelize.authenticate().then(() => {
 });
 
 await sequelize.sync()
-
+app.use(cors('*'));
 const PORT = 5000
 
 app.use(routes); // Use the imported routes
